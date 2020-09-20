1ca2d947d1e87f60b79f537343b84ff1ba1bedde
Nguyễn Thị Thu Hương <37539659+NguyenThuHuongg@users.noreply.github.com>
2020-09-20 15:15:07 +0700

    Add files via upload

--- binomial.py
+++ binomial.py
@@ -9,15 +9,12 @@ Original file is located at
 
 import math
 
-
 def prob(n, p, N):
     tp = math.factorial(N) / (math.factorial(n) * math.factorial(N - n))
     return tp * (p ** n) * ((1 - p) ** (N - n))
 
-
 def infoMeasure(n, p, N):
     return - math.log2(prob(n, p, N))
-
 
 def sumProb(N, p, n):
     sum = 0
@@ -43,40 +40,3 @@ if __name__ == "__main__":
     0.46020538130641064
     2.0392042963658437
   '''
-
-import math
-
-
-def prob(n, p, r):
-    n1_c_r1 = math.factorial(n - 1) / (math.factorial(n - r) * math.factorial(r - 1))
-    return n1_c_r1 * (p ** r) * ((1 - p) ** (n - r))
-
-
-def infoMeasure(n, p, r):
-    return - math.log(prob(n, p, r), 2)
-
-
-def sumProb(N, p, r):
-    s = 0
-    for i in range(r, N + 1):
-        s += prob(i, p, r)
-    return s
-
-
-''' sumProb with N = 5, r = 2, p = 0.5:  0.8125 '''
-''' sumProb with N = 10, r = 2, p = 0.5:  0.9892578125 '''
-''' sumProb with N = 20, r = 2, p = 0.5:  0.9999799728393555 '''
-
-
-def approxEntropy(N, p, r):
-    s = 0
-    for i in range(r, N + 1):
-        s += prob(i, p, r) * infoMeasure(i, p, r)
-    return s
-
-
-''' entropy with N = 10, r = 2, p = 0.5:  2.6178901263724024 '''
-''' entropy with N = 20, r = 2, p = 0.5:  2.7111142477765284 '''
-''' entropy with N = 50, r = 2, p = 0.5: 2.7114687242185123 '''
-
-print(approxEntropy(50, 0.5, 2))
